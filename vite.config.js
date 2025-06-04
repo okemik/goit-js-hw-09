ceff7f4c4e77c1c3ec5bde0acfb2a840e7046f18
okemik <okemik@yahoo.com>
2025-06-05 00:11:32 +0200

    8 th ed

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,11 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    root: 'src',
+    root: '',
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: glob.sync('./*.html'),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
@@ -39,7 +39,7 @@ export default defineConfig(({ command }) => {
     },
     plugins: [
       injectHTML(),
-      FullReload(['./src/**/**.html']),
+      FullReload(['./.html']),
       SortCss({
         sort: 'mobile-first',
       }),

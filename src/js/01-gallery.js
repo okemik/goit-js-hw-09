f9cbb738bd47ef044027e76fc4d4e0796e2af198
okemik <okemik@yahoo.com>
2025-06-05 22:00:06 +0200

    19

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,30 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import { images } from "../data/images";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-const galleryContainer = document.querySelector('.gallery');
 
-// JSON'dan veri al
-fetch('./data/images.json')
-  .then((res) => res.json())
-  .then((images) => {
-    const markup = images
-      .map(
-        ({ preview, original, description }) => `
-        <li class="gallery-item">
-          <a class="gallery-link" href="${original}">
-            <img class="gallery-image" src="${preview}" alt="${description}" />
-          </a>
-        </li>`
-      )
-      .join('');
+// Galeri öğelerini oluşturma
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = images
+  .map(
+    ({ preview, original, description }) => `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${original}">
+        <img 
+          class="gallery-image" 
+          src="${preview}" 
+          alt="${description}" 
+        />
+      </a>
+    </li>
+  `
+  )
+  .join("");
 
-    galleryContainer.innerHTML = markup;
+galleryContainer.innerHTML = galleryMarkup;
 
-    // Lightbox başlat
-    new SimpleLightbox('.gallery a', {
-      captionsData: 'alt',
-      captionDelay: 250,
-    });
-  })
-  .catch((err) => console.error('Görseller yüklenemedi:', err));
+// SimpleLightbox'u başlatma
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt", // Açıklamalar için alt özniteliği
+  captionDelay: 250, // Açıklama 250ms gecikmeli gösterilir
+});
